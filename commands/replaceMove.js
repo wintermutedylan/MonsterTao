4f7f9b13c524b724a03818b9c09dd39ddc800c93
Dylan Wintermute <dylan.wintermute.17@cnu.edu>
2023-11-07 15:08:46 -0500

    added comments to replacemove so I know what to do tomorrow

---
+++ commands/replaceMove.js
@@ -0,0 +1,4 @@
+// use this to replac ea move with another move
+// use addmove code to check for if the pokemon can use the move
+// then just set the move array to the new mvoe array instead of pushign
+// make sure to replace the old move with the new move in the old move array before setting to actually replace the move
