6011877d9f675c4018abdafe44a0c3a7c2d8a4cc
Dylan Wintermute <dylan.wintermute.17@cnu.edu>
2023-11-20 18:19:51 -0500

    added status to show up on party command and removed stats from there. if you want to see stats run unitinfo command

--- interactioncommands/party.js
+++ interactioncommands/party.js
@@ -78,7 +78,7 @@ module.exports = {
         
         
         
-        let status = [];
+        
 
         
         const newEmbed = new EmbedBuilder()
@@ -87,21 +87,35 @@ module.exports = {
         .setDescription(`__**Your Party**__`)
         
         for (let j = 0; j < sorted.length; j++){
+            let status = [];
             if(sorted[j].level != 100){
                 let unitExp = expTable.find(function(item) { return item.name == sorted[j].growthRate});
                 let expToNextLevel = unitExp.levelTable.find(function(expItem) { return expItem.level == sorted[j].level + 1});
-                for(let [key, value] of sorted[j].statusMap){
+                for(var key in sorted[j].statusMap) {
                     if(key != 'sleepTurns' && key != 'confusionTurns'){
-                        if(value) status.push(key);
+                        if(sorted[j].statusMap[key]) status.push(key);
+                        
                     }
                 }
-                console.log(status);
+                // for(let [key, value] of sorted[j].statusMap){
+                //     if(key != 'sleepTurns' && key != 'confusionTurns'){
+                //         if(value) status.push(key);
+                //     }
+                // }
+                if(status.length == 0) status.push("none");
                 newEmbed.addFields(
-                    { name: `PCID# ${sorted[j].pcID}: ${sorted[j].id}`, value: `Level: ${sorted[j].level}, EXP: ${sorted[j].experience}/${expToNextLevel.experience}\nHealth: ${sorted[j].currentHealth}/${sorted[j].health}\nType: ${sorted[j].types.join(", ")}\nStatus: \nMoves: ${sorted[j].moves.join(", ")}`}
+                    { name: `PCID# ${sorted[j].pcID}: ${sorted[j].id}`, value: `Level: ${sorted[j].level}, EXP: ${sorted[j].experience}/${expToNextLevel.experience}\nHealth: ${sorted[j].currentHealth}/${sorted[j].health}\nType: ${sorted[j].types.join(", ")}\nStatus: ${status.join(", ")}\nMoves: ${sorted[j].moves.join(", ")}`}
                 )
             } else {
+                for(var key in sorted[j].statusMap) {
+                    if(key != 'sleepTurns' && key != 'confusionTurns'){
+                        if(sorted[j].statusMap[key]) status.push(key);
+                        
+                    }
+                }
+                if(status.length == 0) status.push("none");
                 newEmbed.addFields(
-                    { name: `PCID# ${sorted[j].pcID}: ${sorted[j].id}`, value: `Level: ${sorted[j].level}, EXP: Max Level\nHealth: ${sorted[j].currentHealth}/${sorted[j].health}\nType: ${sorted[j].types.join(", ")}\nMoves: ${sorted[j].moves.join(", ")}`}
+                    { name: `PCID# ${sorted[j].pcID}: ${sorted[j].id}`, value: `Level: ${sorted[j].level}, EXP: Max Level\nHealth: ${sorted[j].currentHealth}/${sorted[j].health}\nType: ${sorted[j].types.join(", ")}\nStatus: ${status.join(", ")}\nMoves: ${sorted[j].moves.join(", ")}`}
                 )
             }
 
